c55c3b9e1154f593a86c4322d62037bfe539cdf6
alorenacunha <lorenacunha@outlook.com>
2022-11-03 17:02:22 -0300

    feat: :sparkles: try htm to pdf package

--- src/pdf-handler.ts
+++ src/pdf-handler.ts
@@ -12,8 +12,15 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     console.log('This is a log from the lambda, it should show at New Relic');
+    var html_to_pdf = require('html-pdf-node');
+    let options = { format: 'A4' };
+    let file = { content: '<h1>Welcome to html-pdf-node</h1>' };
+
+    const pdfBuffer = html_to_pdf.generatePdf(file, options);
+    console.log('PDF Buffer:-', pdfBuffer);
+
     return {
         statusCode: 200,
-        body: 'It is ok',
+        body: pdfBuffer,
     };
 };
